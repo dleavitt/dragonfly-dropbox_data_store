fc325ab395ae68fd1295c3352a710a4e6ec4dcd2
Daniel Leavitt <daniel.leavitt@gmail.com>
2014-02-04 02:11:38 -0800

    gem boilerplate

---
+++ lib/dragonfly/dropbox_data_store.rb
@@ -0,0 +1,7 @@
+require "dragonfly/dropbox_data_store/version"
+
+module Dragonfly
+  module DropboxDataStore
+    # Your code goes here...
+  end
+end
---
+++ lib/dragonfly/dropbox_data_store/version.rb
@@ -0,0 +1,5 @@
+module Dragonfly
+  module DropboxDataStore
+    VERSION = "0.0.1"
+  end
+end
